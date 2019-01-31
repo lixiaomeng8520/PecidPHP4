39880b569a7d5e45bb26afe9e62bfa0ff5357480
lixiaomeng <lixiaomeng8520@163.com>
2019-01-31 17:39:07 +0800

    add zendframework/zend-diactoros

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
-<?php 
-
+<?php
 require 'vendor/autoload.php';
 
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-    // $r->addRoute('GET', '/users', 'get_all_users_handler');
-    // {id} must be a number (\d+)
-    // $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
-    // The /{title} suffix is optional
-    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
-});
+$stream = new \Zend\Diactoros\Stream('1.txt', 'rw');
+
+var_dump($stream->getMetadata());
